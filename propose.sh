41a48612e50a07eaa1d3d335e1b7280b7d4d7a7e
kien6034 <trinhtrungkientb99@gmail.com>
2024-11-02 09:25:38 +0700

    feat: update proposal

---
+++ propose.sh
@@ -0,0 +1,4 @@
+# terrad query gov params --node https://rpc.terra-classic.hexxagon.io:443
+
+# terrad tx gov submit-proposal draft_proposal.json --from main --node https://rpc.terra-classic.hexxagon.io:443 
+terrad tx gov submit-proposal draft_proposal.json --from orbitlabs --node https://rpc.terra-classic.hexxagon.io:443  --chain-id columbus-5 --fees 5665000uluna
