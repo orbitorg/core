61a0832ed1645893a17c0f363ac3e0c6f48d1aea
Tuan Tran <tropicaldog17@gmail.com>
2025-10-15 21:33:14 +0700

    fix test

--- cmd/terrad/main.go
+++ cmd/terrad/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"os"
 
+	core "github.com/classic-terra/core/v3/types"
 	terraapp "github.com/classic-terra/core/v3/app"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 )
+
+func init() {
+	// Override the SDK's default bond denom to match Terra's configuration
+	sdk.DefaultBondDenom = core.MicroLunaDenom
+}
 
 func main() {
 	rootCmd, _ := NewRootCmd()
